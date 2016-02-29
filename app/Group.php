edcbccea1316e7677275654b4718f7700b4e73c7
Sven Luijten <svenluijten@outlook.com>
2016-02-29 16:39:46 +0100

    Add active field to the groups table

--- app/Group.php
+++ app/Group.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    /**
+     * The fields that may be mass assigned.
+     *
+     * @var array
+     */
     protected $fillable = ['name'];
+
+    /**
+     * Scope if the group is active.
+     *
+     * @param  \Illuminate\Database\Query\Builder $query
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('active', true);
+    }
 }
