e9c63d40ab6f7232e4932575cd1443d827e92514
Sven Luijten <svenluijten@outlook.com>
2016-03-07 10:21:58 +0100

    remove constructor on AuthController

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -8,44 +8,28 @@ use Illuminate\Http\Request;
 use App\Utilities\ApiResponse;
 use App\Http\Controllers\Controller;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use Illuminate\Contracts\Routing\ResponseFactory;
 
 class AuthController extends Controller
 {
-    /**
-     * @var \Illuminate\Contracts\Routing\ResponseFactory
-     */
-    protected $response;
-
-    /**
-     * Instantiate the AuthController.
-     *
-     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
-     */
-    public function __construct(ApiResponse $response)
-    {
-        $this->response = $response;
-    }
-
     /**
      * Authenticate the user when given valid credentials.
      *
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function authenticate(Request $request)
+    public function authenticate(Request $request, ApiResponse $response)
     {
         $credentials = $request->only('email', 'password');
 
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
-                return $this->response->make()->notAuthorized();
+                return $response->make()->notAuthorized();
             }
         } catch (JWTException $e) {
-            return $this->response->make()->internalError('Could not create authentication token.');
+            return $response->make()->internalError('Could not create authentication token.');
         }
 
-        return $this->response->make([
+        return $response->make([
             'token' => $token,
         ]);
     }
